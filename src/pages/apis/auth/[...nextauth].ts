3b34f2d1382c973f8c23869d82e1d37b942f7464
junho <anc5557@naver.com>
2023-11-16 14:59:36 +0900

    Add GitHub authentication provider and update image domains.

--- src/pages/apis/auth/[...nextauth].ts
+++ src/pages/apis/auth/[...nextauth].ts
@@ -1,12 +1,17 @@
 // path : mealchoice/src/pages/apis/auth/[...nextauth].ts
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import  GithubAuthProvider from "next-auth/providers/github";
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
+    }),
+    GithubAuthProvider({
+      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID!,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET!,
     }),
   ],
 };
