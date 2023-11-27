83969b8d2d169e91693b714b595d987ec8437658
junho <anc5557@naver.com>
2023-11-27 13:37:32 +0900

    Fix import formatting in verifyAuthToken middleware

--- src/middleware/verifyAuthToken.ts
+++ src/middleware/verifyAuthToken.ts
@@ -1,16 +1,20 @@
 // path: mealchoice/src/middleware/verifyAuthToken.ts
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import { parseCookies } from 'nookies';
-import { admin } from '@/firebase/firebaseAdmin';
+import { NextApiRequest, NextApiResponse } from "next";
+import { parseCookies } from "nookies";
+import { admin } from "@/firebase/firebaseAdmin";
 
 // 미들웨어 함수를 생성합니다. next는 선택적으로 사용할 수 있습니다.
-export const verifyAuthToken = async (req: NextApiRequest, res: NextApiResponse, next?: () => void) => {
+export const verifyAuthToken = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next?: () => void
+) => {
   const cookies = parseCookies({ req });
   const token = cookies.authToken;
-  
+
   if (!token) {
-    return res.status(401).json({ message: '인증 토큰이 쿠키에 없습니다.' });
+    return res.status(401).json({ message: "인증 토큰이 쿠키에 없습니다." });
   }
 
   try {
@@ -18,6 +22,6 @@ export const verifyAuthToken = async (req: NextApiRequest, res: NextApiResponse,
     (req as any).user = decodedToken;
     next && next();
   } catch (error) {
-    return res.status(403).json({ message: '유효하지 않은 토큰입니다.' });
+    return res.status(403).json({ message: "유효하지 않은 토큰입니다." });
   }
 };
