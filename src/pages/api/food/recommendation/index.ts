cb53ddbe6370bbb59b4c9470b852995ce8f31057
junho <anc5557@naver.com>
2023-12-01 09:39:46 +0900

    Refactor food recommendation API prompt formatting

--- src/pages/api/food/recommendation/index.ts
+++ src/pages/api/food/recommendation/index.ts
@@ -1,16 +1,6 @@
 // path : pages/api/food/recommendation/index.ts
 
 // 음식 추천 api
-// POST /api/food/recommendation
-// gpt4를 이용하여 음식 추천
-// 입력으로 카테고리(한식, 중식, 일식, 양식, 분식), 시간대(아침, 점심, 저녁, 야식)을 받는다.
-// 현재 날짜를 기준으로 제외기간 안에 있는 음식은 추천하지 않는다.
-// 좋아하는 음식은 추천하고 싫어하는 음식은 추천하지 않는다.
-// gpt 프롬프트 예시
-// 최근에 음식과 싫어하는 음식을 제외하고 좋아하는 음식을 보고 {점심}에 먹을 {한식}을 추천해주세요.
-// 최근에 먹은 음식 : "김치찌개", "된장찌개", "김치볶음밥", "김치전"
-// 싫어하는 음식 : "콩나물국밥", "라면", "떡볶이"
-
 import { NextApiRequest, NextApiResponse } from "next";
 import { verifyAuthToken } from "@/middleware/verifyAuthToken";
 import { admin } from "@/firebase/firebaseAdmin";
@@ -78,7 +68,8 @@ const handler = async (req: NextApiRequestWithUser, res: NextApiResponse) => {
 
       // gpt4
       // 프롬프트
-      const prompt = `최근에 먹은 음식 : ${exclusionFoods}, 싫어하는 음식 : ${hateFoods} , json 형식 : {menu: "추천 음식 이름", description: "추천 이유 설명"}`;
+      const prompt = `최근에 먹은 음식 : ${exclusionFoods}, 싫어하는 음식 : ${hateFoods} 
+      , json 형식 : {menu: "추천 음식 이름", description: "추천 이유 설명"}`;
 
       try {
         const response = await openai.chat.completions.create({
@@ -90,7 +81,8 @@ const handler = async (req: NextApiRequestWithUser, res: NextApiResponse) => {
             },
             {
               role: "user",
-              content: `최근에 음식과 싫어하는 음식을 제외하고 좋아하는 음식을 보고 ${time}에 먹을 ${category}을 추천해주세요.`,
+              content: `최근에 음식과 싫어하는 음식을 제외하고 좋아하는
+               음식을 보고 ${time}에 먹을 ${category}을 추천해주세요.`,
             },
           ],
           response_format: { type: "json_object" },
