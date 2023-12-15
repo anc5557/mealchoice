1565b272541371a505aa44d85437f2cbbaf086bd
junho <anc5557@naver.com>
2023-12-15 15:04:21 +0900

    Update favicon link in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,8 +22,7 @@ const InnerComponent = ({ Component, pageProps, router }: AppProps) => {
       <Head>
         <title>Meal Choice</title>
         <meta name="description" content="Meal Choice" />
-        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
-        <link rel="alternate icon" href="/logo.png" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       <div className="min-h-screen">
         <Component {...pageProps} />
